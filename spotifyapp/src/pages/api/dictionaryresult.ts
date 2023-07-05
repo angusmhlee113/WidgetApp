564b45566b8be1b34c9c0b1f23aa4cda37de55fc
Angus Lee <angusmhlee@gmail.com>
2023-07-05 09:13:33 +0800

    FE of medical dictionary display properly

--- spotifyapp/src/pages/api/dictionaryresult.ts
+++ spotifyapp/src/pages/api/dictionaryresult.ts
@@ -2,16 +2,22 @@ import axios from "axios";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const dictionaryresult = async (req: NextApiRequest, res: NextApiResponse) => {
-  let result: Array<JSON> = [];
+  let result: any = [];
   if (req.method === "GET") {
     try {
-      const { word } = req.query;
+      const word = req.query.word;
       const response = await axios.get(
-        `https://dictionaryapi.com/api/v3/references/medical/json/doctor?key=a743a27b-3e0d-40bf-a714-f2f9d069a7e7`
+        `https://dictionaryapi.com/api/v3/references/medical/json/${word}?key=a743a27b-3e0d-40bf-a714-f2f9d069a7e7`
       );
       for (let i = 0; i < response.data.length; i++) {
         if (response.data[i].meta.id.includes(":")) {
-          result.push(response.data[i]);
+          //sort through the data
+          const sortedresult = {
+            word: response.data[i].meta.id.split(":")[0],
+            definition: response.data[i].shortdef[0],
+            category: response.data[i].fl,
+          };
+          result.push(sortedresult);
         }
       }
       if (response.data.length > 0) {
--- spotifyapp/src/pages/medicaldictionary/index.tsx
+++ spotifyapp/src/pages/medicaldictionary/index.tsx
@@ -1,10 +1,42 @@
 import MedicalDiction from "~/components/medicaldictionary/medicaldiction";
-
+import { useState, useEffect } from "react";
+import { set } from "zod";
 const MedicalDictionary = () => {
+  const [medicalDictions, setMedicalDictions] = useState("");
+
+  const onChangeHandler = (e: any) => {
+    setMedicalDictions(e.target.value);
+  };
+
+  const onClickHandler = async () => {
+    try {
+      const data = await fetch(
+        `${window.location.origin}/api/dictionaryresult?word=${medicalDictions}`
+      );
+      const json = await data.json();
+      console.log(json);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <h1 className="text-gray-500">Medical Dictionary</h1>
-      <MedicalDiction />
+      <div className="flex flex-col ">
+        <input
+          id={"search"}
+          className="button_password mb-2 w-2/3 grow-[2] rounded-xl border-black p-2 outline-black drop-shadow-lg"
+          onChange={onChangeHandler}
+        />
+        <button
+          className="w-1/3 rounded-xl border-black outline-black"
+          onClick={onClickHandler}
+        >
+          Search
+        </button>
+      </div>
+      <p>{medicalDictions}</p>
     </div>
   );
 };
